8e79d6eab20f927074360721422eea83fd4bb91e
AksaGeorge44 <85211463+AksaGeorge44@users.noreply.github.com>
2022-11-11 15:48:37 +0530

    commit

--- app.js
+++ app.js
@@ -2,12 +2,49 @@ var express=require('express')
 var bodyParser=require('body-parser')
 var mongoose=require('mongoose')
 var{libraryModel}=require('./models/libraryModels')
-mongoose.connect("",{useNewUrlParser:true})
+mongoose.connect("mongodb+srv://Aksageorge:aksageorge44@cluster0.3ictqpt.mongodb.net/?retryWrites=true&w=majority",{useNewUrlParser:true})
 var app=express()
 app.use(bodyParser.urlencoded({ extended: false }))
 
 app.use(bodyParser.json())
-
+app.get('/viewall',async(req,res)=>{
+try{
+    var result=await libraryModel.find()
+    res.json(result)
+}
+catch(error){
+res.send(error)
+}
+})
+app.post('/search',async(req,res)=>{
+    try{
+        var result=await libraryModel.find(req.body)
+        res.json(result)
+    }
+    catch(error){
+        res.json({"status":"error"})
+    }
+})
+app.post('/edit',async(req,res)=>{
+    try{
+        var result=await libraryModel.findByIdAndUpdate({"_id":req.body._id},req.body)
+    }
+    catch(error)
+    
+    {
+        res.json({"status":"error"})
+    }
+})
+app.post('/delete',async(req,res)=>{
+    try{
+        var result=await libraryModel.findByIdAndDelete({"_id":req.body._id},req.body)
+    }
+    catch(error)
+    
+    {
+        res.json({"status":"error"})
+    }
+})
 app.get('/',(req,res)=>{
     res.send("hello students")
 })
